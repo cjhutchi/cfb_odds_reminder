c09668621ff7bd17d00b3c7aad5a58b96315588f
cjhutchi <chris@chriss-air.lan>
2022-08-09 22:58:09 -0400

    Adds ranking api call

---
+++ app/api/cfb_data_api/client.rb
@@ -0,0 +1,29 @@
+module CfbDataApi
+  class Client
+    POLL_NAME = "AP Top 25".freeze
+
+    def self.get_rankings
+      api_key = Rails.application.credentials.dig(:cfb_data_api, :api_key)
+      # Currently no rankings yet
+      # current_year = Time.zone.now.year
+      current_year = 2021
+      current_week = Week.current.number
+
+      uri = URI("https://api.collegefootballdata.com/rankings?year=#{current_year}&week=#{current_week}&seasonType=regular")
+
+      parse_rankings(
+        JSON.parse(
+          Net::HTTP.get(uri, {'Authorization' => "Bearer #{api_key}"})
+        )
+      )
+    end
+
+    private
+
+    def self.parse_rankings(response)
+      response.first["polls"].find do |poll|
+        poll["poll"] == POLL_NAME
+      end["ranks"]
+    end
+  end
+end
