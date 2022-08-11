05d5daf668b407b8fcd99509e2fdfabf87382bdf
cjhutchi <chris@chriss-air.lan>
2022-08-10 22:15:33 -0400

    Make it so only top 25 is shown

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,20 @@
 class GamesController < ApplicationController
   def current_week
     games = Game.games_for_current_week.map do |game|
-      {
-        home_team: "#{game.home_team.school} #{game.home_team.mascot}",
-        home_team_rank: rank(game.home_team.school),
-        away_team: "#{game.away_team.school} #{game.away_team.mascot}",
-        away_team_rank: rank(game.away_team.school),
-        commence_time: game.commence_time,
-        home_team_points: game.home_team_points,
-        away_team_points: game.away_team_points
-      }
+      home_team_rank = rank(game.home_team.school)
+      away_team_rank = rank(game.away_team.school)
+
+      if home_team_rank.present? || away_team_rank.present?
+        {
+          home_team: "#{game.home_team.school} #{game.home_team.mascot}",
+          home_team_rank: home_team_rank,
+          away_team: "#{game.away_team.school} #{game.away_team.mascot}",
+          away_team_rank: away_team_rank,
+          commence_time: game.commence_time,
+          home_team_points: game.home_team_points,
+          away_team_points: game.away_team_points
+        }
+      end
     end
 
     response = {
